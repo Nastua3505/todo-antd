1e80e3653f0a8c65edb6f68eb80db552fb10eebc
Nastua3505 <nastuaaas@mail.ru>
2022-09-18 17:57:28 +0300

    done

---
+++ src/components/CustomInput/index.js
@@ -0,0 +1,32 @@
+import { Input, Button,Row,Col} from 'antd';
+import{useState } from 'react';
+
+const CustomInput= ({addTodo})=>{
+
+    const[text, setText]= useState("")
+    
+    const handleClick=()=>{
+addTodo(text);
+setText("");
+    };
+
+    return(
+        
+        <Row>
+        <Col offset={8}span={8}>
+            <Input onChange={(e)=> setText(e.target.value)} 
+    value={text}
+    placeholder="Basic usage" 
+    onPressEnter={()=> handleClick()}
+    />
+    
+    </Col>
+        <Col span={1}>
+            <Button  type="primary" onClick={()=> handleClick()} >Add ToDo </Button>
+    </Col>
+      </Row>
+    
+    )}
+    
+    export default CustomInput;
+    
